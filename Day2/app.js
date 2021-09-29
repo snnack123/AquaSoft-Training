fbc0930a17cf1d706e1febd6f3f7eb93e94d9e6f
Lungu Mihai-Adrian <75578553+snnack123@users.noreply.github.com>
2021-09-29 11:10:01 +0300

    Add files via upload

---
+++ Day2/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const Sequelize = require("sequelize");
+const db = new Sequelize("aquasoft", "root", "5630", {
+    host: "localhost",
+    dialect: "mysql",
+    operatorsAliases: false,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+});
+
+//Test DB
+db.authenticate()
+    .then(() => console.log("Database connected..."))
+    .catch((err) => console.log("Error", err));
+const app = express();
+
+app.get("/", (req, res) => res.send("INDEX"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
