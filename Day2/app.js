58256cba979462a637d53a222434e6d724f78f09
Lungu Mihai-Adrian <75578553+snnack123@users.noreply.github.com>
2021-09-30 16:34:03 +0300

    Add files via upload

--- Day2/app.js
+++ Day2/app.js
@@ -4,13 +4,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 // Database
-
-const db = require("./config/database");
-
-// Test if DataBase is Connected
-db.authenticate()
-    .then(() => console.log("Database connected..."))
-    .catch((err) => console.log("Error", err));
+const db = require("./models");
 
 const app = express();
 app.use(bodyParser.json()); // Stie sa interpreteze obiectul trimis
@@ -18,8 +12,13 @@ app.use(bodyParser.json()); // Stie sa interpreteze obiectul trimis
 app.get("/", (req, res) => res.send("INDEX"));
 
 app.use("/employees", require("./routes/employees"));
+app.use("/projects", require("./routes/projects"));
 
 // Localhost port to test app
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+db.sequelize.sync();
+
+module.exports = db;
