f37c7ecb910a3295ee640af13c54c2cf1b0e6734
Lungu Mihai-Adrian <75578553+snnack123@users.noreply.github.com>
2021-09-30 16:35:10 +0300

    Add files via upload

--- Day2/models/Employee.js
+++ Day2/models/Employee.js
@@ -1,27 +1,35 @@
 const Sequelize = require("sequelize");
-const db = require("../config/database");
-
 // Ce atribute vreau sa afisez din tabela angajati, si in ce ordine
-const Employees = db.define("employee", {
-    Name: {
-        type: Sequelize.STRING,
-    },
-    Adress: {
-        type: Sequelize.STRING,
-    },
-    Email: {
-        type: Sequelize.STRING,
-    },
-    Hire_date: {
-        type: Sequelize.STRING,
-    },
-    Salary: {
-        type: Sequelize.STRING,
-    },
-    Job_Title: {
-        type: Sequelize.STRING,
-    },
-});
+module.exports = (sequelize, Sequelize) => {
+    const Employees = sequelize.define("employee", {
+        Id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoincrement: true,
+        },
+        Name: {
+            type: Sequelize.STRING,
+        },
+        Adress: {
+            type: Sequelize.STRING,
+        },
+        Email: {
+            type: Sequelize.STRING,
+        },
+        Hire_date: {
+            type: Sequelize.DATE,
+        },
+        Salary: {
+            type: Sequelize.STRING,
+        },
+        Job_Title: {
+            type: Sequelize.STRING,
+        },
+        projectId: {
+            type: Sequelize.INTEGER,
+        },
+    });
+    return Employees;
+};
 
 // Transmit ceea ce am extras catre alta pagina
-module.exports = Employees;
