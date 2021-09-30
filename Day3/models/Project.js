6672d68e730511a2cf4e090c651e86d5dad0f95b
Lungu Mihai-Adrian <75578553+snnack123@users.noreply.github.com>
2021-09-30 16:36:39 +0300

    Add files via upload

--- Day3/models/Project.js
+++ Day3/models/Project.js
@@ -1,24 +1,23 @@
 const Sequelize = require("sequelize");
-const db = require("../config/database");
 
 // Ce atribute vreau sa afisez din tabela de proiecte, si in ce ordine
-const Projects = db.define("project", {
-    Project_name: {
-        type: Sequelize.STRING,
-    },
-    Start_date: {
-        type: Sequelize.DATE,
-    },
-    Planned_end_date: {
-        type: Sequelize.DATE,
-    },
-    Description: {
-        type: Sequelize.STRING,
-    },
-    Project_code: {
-        type: Sequelize.INTEGER,
-    },
-});
-
-// Transmit ceea ce am extras catre alta pagina
-module.exports = Projects;
+module.exports = (sequelize, Sequelize) => {
+    const Projects = sequelize.define("project", {
+        Project_name: {
+            type: Sequelize.STRING,
+        },
+        Start_date: {
+            type: Sequelize.DATE,
+        },
+        Planned_end_date: {
+            type: Sequelize.DATE,
+        },
+        Description: {
+            type: Sequelize.STRING,
+        },
+        Project_code: {
+            type: Sequelize.INTEGER,
+        },
+    });
+    return Projects;
+};
